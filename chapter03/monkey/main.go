2f9ed4da38861cf97e99e1391fdfc509ab45d7f0
cc <kk@mail.com>
2024-10-31 21:06:17 +0800

    完成基础的eval：int，bool，数学符号、if

---
+++ chapter03/monkey/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	current, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s!\n", current.Username)
+	fmt.Println("Welcome to the Monkey programming language!")
+	repl.Start(os.Stdin, os.Stdout)
+}
---
+++ chapter03/monkey/repl/repl.go
@@ -0,0 +1,40 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/evaluator"
+	"monkey/lexer"
+	"monkey/parser"
+)
+
+const PROMPT = ">>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			fmt.Println("Whoops! Parsing Errors:")
+			for _, msg := range p.Errors() {
+				fmt.Println("\t", msg)
+			}
+			continue
+		}
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
