6d0823cd560912d24c109963a3bf47f6d44cbc4f
cc <kk@mail.com>
2024-11-02 12:53:47 +0800

    完成第3章，基本的解释语言实现

---
+++ chapter03/monkey/object/environment.go
@@ -0,0 +1,28 @@
+package object
+
+type Environment struct {
+	store map[string]Object
+	outer *Environment
+}
+
+func NewEnclosedEnvironment(out *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = out
+	return env
+}
+
+func NewEnvironment() *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s, outer: nil}
+}
+func (e *Environment) Get(name string) (Object, bool) {
+	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
+	return obj, ok
+}
+func (e *Environment) Set(name string, val Object) Object {
+	e.store[name] = val
+	return val
+}
